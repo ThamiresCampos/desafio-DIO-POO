85eb676ff8b759fe2d98298aacdb7ea015883a48
Thamires Campos <thamires_campos29@hotmail.com>
2023-03-28 21:36:41 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,9 +19,58 @@ public class Main {
     mentoria.setDescricao("descrição mentoria de java");
     mentoria.setData(LocalDate.now());
 
-    System.out.println(curso1);
+    /*System.out.println(curso1);
     System.out.println(curso2);
-    System.out.println(mentoria);
+    System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setNome("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devThamires = new Dev();
+        devThamires.setNome("Thamires");
+        devThamires.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Thamires: " + devThamires.getConteudosInscritos());
+        devThamires.progredir();
+        devThamires.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Thamires: " + devThamires.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Thamires: " + devThamires.getConteudosConcluidos());
+        System.out.println("xp: " + devThamires.calcularTotalXp());
+
+        System.out.println();
+        System.out.println("=================================");
+        System.out.println();
+
+        Dev devCamilly = new Dev();
+        devCamilly.setNome("Camilly");
+        devCamilly.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camilly: " + devCamilly.getConteudosInscritos());
+        devCamilly.progredir();
+        devCamilly.progredir();
+        devCamilly.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos Camilly: " + devCamilly.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camilly: " + devCamilly.getConteudosConcluidos());
+        System.out.println("xp: " + devCamilly.calcularTotalXp());
+
+        System.out.println();
+        System.out.println("=================================");
+        System.out.println();
+
+        Dev devBianca = new Dev();
+        devBianca.setNome("Bianca");
+        devBianca.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Bianca: " + devBianca.getConteudosInscritos());
+        devBianca.progredir();
+        devBianca.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos Bianca: " + devBianca.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Bianca: " + devBianca.getConteudosConcluidos());
+        System.out.println("xp: " + devBianca.calcularTotalXp());
 
 
 
